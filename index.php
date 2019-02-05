c7edd324fc15797493b65cff39667c43c0848eac
Tristan JONES <tjones@c2r5s2.wethinkcode.co.za>
2019-02-05 18:48:54 +0200

    5th

--- index.php
+++ index.php
@@ -17,8 +17,9 @@
         <title>Camagru</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="./style/index.css">
-        <link rel="stylesheet" type="text/css" href="./webcam.js">
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <script src="capture.js">
+        </script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
     </head>
@@ -40,7 +41,8 @@
         </div>
         <p>&nbsp;</p>
         <div id="camera" class="loginbox">
-            <video id="video">Video No Work</video>
+            <video id="video" autoplay="true">Video stream not available.</video>
+            <button id="startbutton">Take photo</button>
         </div>
         <p>&nbsp;</p>
         <div id="upload" class="loginbox">
@@ -49,135 +51,17 @@
             <form method="post" action="functions/merge.php" enctype='multipart/form-data'>
                            <!-- <input type='file' name='image'/>
                            <input type='submit' value='insert' name='insert'> -->
+                           <label for="sticker">Stickers</label>
+                           <select id="sticker" name="sticker">
+                                <option value="./images/old_beard.jpg">Old Beard</option>
+                                <option value="">None</option>
+                           </select>
                            <input type="file" name='insert'/>
                            <input type="submit"/>Upload
             </form>
         </div>
         <p>&nbsp;</p>
-        <div class="box">
-            <form id="export" action="functions/merge.php" method="POST" enctype="multipart/form-data">
-                <input type="file" name="image_1" id="merged_image" value="">
-                <input type="file" name="image_2" id="image_2" value="">
-                <button type="submit" class="btn" name="exp_photo" id="export_photo" value="OK">Upload</button>
-            </form>
-        </div>
         <script>
-            var width = 320;
-            var height = 640;
-            var stream = true;
-            var video = document.getElementById('video');
-            var canvas = document.getElementById('canvas');
-            var photo = document.getElementById('photo');
-            var start = document.getElementById('start');
-            var cam = document.getElementById('cam');
-            var cam_file_sel = document.getElementById('cam_file');
-            var test = document.getElementById('test');
-            document.querySelector('#sticker_image').src = document.getElementById('sticker_select').value;
-            document.getElementById('image_upload').hidden = true;
-            var img2 = document.getElementById('sticker_image');
-            var image2 = document.getElementById('image_2');
-
-                navigator.mediaDevices.getUserMedia({
-                    video: true,
-                    audio: false
-                })
-                .then(function(stream) {
-                    video.srcObject = stream;
-                    video.play();
-                })
-                .catch(function(err) {
-                    console.log("Error: " + err);
-                })
-                video.addEventListener('canplay', function(ev) {
-                    if (!streaming) {
-                        height = video.videoHeight / (video.videoWidth / width);
-
-                        video.setAttribute('width', 320);
-                        video.setAttribute('height', 320);
-                        canvas.setAttribute('width', width);
-                        canvas.setAttribute('height', height);
-                        streaming = true;
-                    }
-                }, false);
-
-                function web_upload() {
-                    if (cam_file_sel.value == "file") {
-                        document.getElementById('camera').hidden = true;
-                        document.getElementById('image_upload').hidden = false;
-                    }
-                    else {
-                        document.getElementById('camera').hidden = false;
-                        document.getElementById('image_upload').hidden = true;
-                    }
-                }
-
-                setInterval(web_upload(),300);
-
-                
-                function takepicture() {
-                    var exportBtn = document.getElementById('export');
-                    image2.value = img2.src;
-                    exportBtn.style.display = 'block';
-                    if (cam_file_sel.value == "camera") {
-                        var context = canvas.getContext('2d');
-                        if (width && height) {
-                            canvas.width = width;
-                            canvas.height = height;
-                            context.drawImage(video, 0, 0, width, height);
-                            var data = canvas.toDataURL();
-                            document.getElementById('merged_image').value = data;
-                        }
-                    }
-                    else if (cam_file_sel.value == "file") {
-                        var context = canvas.getContext('2d');
-                        if (width && height) {
-                            canvas.width = width;
-                            canvas.height = height;
-                            var img = document.getElementById('upload_image');
-                            context.drawImage(img, 0, 0, width, height);
-                            var data = canvas.toDataURL();
-                            document.getElementById('merged_image').value = data;
-                        }
-                    }
-                    else {
-                        clearphoto();
-                    }
-                }
-                
-                function clearphoto() {
-                        var context = canvas.getContext('2d');
-                        context.fillStyle = "#AAA";
-                        context.fillRect(0, 0, canvas.width, canvas.height);
-                        var data = canvas.toDataURL('image/png');
-                        photo.setAttribute('src', data);
-                    }
-                    start.addEventListener('click', function(ev) {
-                        takepicture();
-                        ev.preventDefault();
-                    }, false);
-                    cam_file_sel.addEventListener('change', function() {
-                        if (cam_file_sel.value == "file") {
-                            document.getElementById('camera').hidden = true;
-                            document.getElementById('image_upload').hidden = true;
-                        }
-                    }, false);
-
-                    document.querySelector('#img_upload').addEventListener('change', function() {
-                        if (this.files.length === 0)
-                            return;
-                        var file = this.files[0];
-                        var reader = new FileReader();
-                        reader.onload = function(e) {
-                            document.querySelector('#upload_image').src = e.target.result;
-                        };
-                        reader.readAsDataURL(file);
-                    }, false);
-
-
-                document.querySelector('#sticker_select').addEventListener('change', function() {
-                    document.querySelector('#sticker_image').src = document.getElementById('sticker_select').value;
-                    var reader = new FileReader();
-                }, false)
         </script>
     </body>
 </html>
